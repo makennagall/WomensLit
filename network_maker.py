6622d88ff4196c27f6f59e6effbeb62dde8e6fcc
makennagall <68980127+makennagall@users.noreply.github.com>
2023-03-28 17:34:41 -0400

    network creator initialized

---
+++ network_maker.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from pyvis.network import Network 
+net = Network()
+
+import pandas as pd
+
+
+#create library of word connections:
+reeves_text = open("ReevesTale.txt")
+cleanedReeve = ''
+for line in reeves_text:
+    line = line.lower()
+    if line != "\n":
+        for word in line.split(' '):
+            word = ''.join(x for x in word if x.isalpha())
+            if word != '':
+                cleanedReeve = cleanedReeve + ' '+ word
+print(cleanedReeve)
+wordDict = {}
+clean_r_list = cleanedReeve.split(' ')
+for index, word in enumerate(clean_r_list):
+    if index < len(clean_r_list)-1:
+        if word in wordDict:
+            wordDict[word].append(clean_r_list[index + 1])
+        else:
+            wordDict[word] = [clean_r_list[index + 1]]
+print(wordDict)
+for key in wordDict:
+    net.add_node(key, label = key)
+for key in wordDict:
+    for word in wordDict[key]:
+        net.add_edge(key, word)    
+net.show('mygraph.html')
